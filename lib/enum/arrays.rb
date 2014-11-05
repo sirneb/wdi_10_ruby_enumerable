03545ffda05643a76446485c7587babae52ecb8a
David Fisher <tibbon@gmail.com>
2014-11-05 10:01:51 -0500

    Array spec working

--- lib/enum/arrays.rb
+++ lib/enum/arrays.rb
@@ -1,44 +1,45 @@
-class ArrayLab
-  def initialize(array=[])
-    @array = array
-  end
+class Array
 
   def sum_of_numeric_elements
-    array
+    self
     .select {|element| (element.class == Fixnum) || (element.class == Float) }
     .inject(&:+)
   end
 
   def product_of_numeric_elements
-    array
+    self
     .select {|element| (element.class == Fixnum) || (element.class == Float) }
-    .inject(&:+)
+    .inject(&:*)
   end
 
   def even_numeric_elements
     self
+    .select {|element| element.class == Fixnum }
+    .select(&:even?)
   end
 
   def odd_numeric_elements
     self
+      .select {|element| element.class == Fixnum }
+      .select(&:odd?)
   end
 
   def string_elements
     self
+      .select {|element| element.class == String }
   end
 
-  def uppercase_all_lowercase_string_elements
+  def lowercase_string_elements_as_uppercase
     self
+      .select {|element| element.class == String }
+      .select {|element| element[0].match(/[a-z]/) }
+      .map {|element| element[0].upcase + element.slice(1..element.length)}
   end
 
   def capitalized_elements
     self
-  end
-
-  private
-
-  def array
-    @array
+      .select {|element| element.class == String }
+      .select {|element| element[0].match(/[A-Z]/) }
   end
 
 end
--- spec/arrays_spec.rb
+++ spec/arrays_spec.rb
@@ -3,23 +3,52 @@ require_relative '../lib/enum'
 
 RSpec.describe 'array methods' do
   before(:each) do
-    test_array = [1, 2, 3, 7, 10, 'foobar', false, 'Boston', 'New York', 'General Assembly', 17]
-    @array_lab = ArrayLab.new(test_array)
+    test_array = [1, 2, 3, 7, 10, 1.1, 'ruby', 'python', false, 'Boston', 'New York', 'General Assembly', 17]
+    @array = Array.new(test_array)
   end
 
   describe '#sum_of_numeric_elements' do
     it 'sums all Float and Fixnum elements' do
-      expect(@array_lab.sum_of_numeric_elements).to be 40
+      expect(@array.sum_of_numeric_elements).to be 41.1
     end
   end
 
-  # describe '#prompt_input' do
-  #   it 'does something' do
-  #     game = Game.new
-  #     # game.stub(:gets).with('go north') # {'go north'}
-  #     allow(Kernel).to receive(:gets)
-  #     expect(game.one_command).to eq("It is just another day, then.")
-  #   end
-  # end
+  describe '#product_of_numeric_elements' do
+    it 'multiplies all Float and Fixnum elements' do
+      expect(@array.product_of_numeric_elements.floor).to be 7854
+    end
+  end
+
+  describe '#even_numeric_elements' do
+    it 'returns only even Fixnum elements' do
+      expect(@array.even_numeric_elements).to eq [2, 10]
+    end
+  end
+
+
+  describe '#odd_numeric_elements' do
+    it 'returns only odd Fixnum elements' do
+      expect(@array.odd_numeric_elements).to eq [1, 3, 7, 17]
+    end
+  end
+
+  describe '#string_elements' do
+    it 'returns only String elements' do
+      expect(@array.string_elements).to eq ["ruby", "python", "Boston", "New York", "General Assembly"]
+    end
+  end
+
+  describe '#lowercase_string_elements_as_uppercase' do
+    it 'returns String elements that are lowercase in uppercase' do
+      expect(@array.lowercase_string_elements_as_uppercase).to eq ["Ruby", "Python"]
+    end
+  end
+
+
+  describe '#capitalized_elements' do
+    it 'returns String elements that are uppercase' do
+      expect(@array.capitalized_elements).to eq ["Boston", "New York", "General Assembly"]
+    end
+  end
 
 end
