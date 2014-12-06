02a2c89e7f8f918fc529cb51b1173b7c8968c57f
Frank Kwok <frankkwok@gmail.com>
2014-12-06 11:35:36 -0500

    finish arrays

--- lib/enum/arrays.rb
+++ lib/enum/arrays.rb
@@ -1,24 +1,31 @@
 class Array
 
   def sum_of_numeric_elements
+    self.keep_if {|i| [Float, Fixnum].include? i.class}.reduce(&:+)
   end
 
   def product_of_numeric_elements
+    self.keep_if {|i| [Float, Fixnum].include? i.class}.reduce(&:*)
   end
 
   def even_numeric_elements
+    self.keep_if {|i| Fixnum == i.class && i.even?}
   end
 
   def odd_numeric_elements
+    self.keep_if {|i| Fixnum == i.class && i.odd?}
   end
 
   def string_elements
+    self.keep_if {|i| String == i.class}
   end
 
   def lowercase_string_elements_as_uppercase
+    self.keep_if {|i| String == i.class && i[0] =~ /[a-z]/}.map(&:capitalize)
   end
 
   def capitalized_elements
+    self.keep_if {|i| String == i.class && i[0] =~ /[[:upper:]]/}
   end
 
 end
