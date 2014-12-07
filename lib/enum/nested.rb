4f44afb3cfb5f7873d9b24a2ea63afa5f3ea7351
Frank Kwok <frankkwok@gmail.com>
2014-12-06 23:32:50 -0500

    refactor nested solutions

--- lib/enum/nested.rb
+++ lib/enum/nested.rb
@@ -18,43 +18,38 @@ class People
   end
 
   def ages_sum
-    @people.reduce(0) {|memo, p| memo + p.age}
+    people.reduce(0) {|memo, p| memo + p.age}
   end
 
   def average_age
-    ages_sum / @people.count
+    ages_sum / people.count
   end
 
   def total_years_programming_experience_for_all_languages
-    @people.reduce([]) { |memo, p| memo + p.years_language_experience.values }
+    people.reduce([]) { |memo, p| memo + p.years_language_experience.values }
            .reduce(&:+)
   end
 
   def favorite_food_frequency
-    @people.reduce([]) { |memo, p| memo + p.favorite_foods }
+    people.reduce([]) { |memo, p| memo + p.favorite_foods }
            .each_with_object(Hash.new(0)) { |c, obj| obj[c] += 1 }
   end
 
   def total_combined_years_language_experience(language)
-    @people.reject do |p|
+    people.reject do |p|
       p.years_language_experience[language] == nil
     end
-    .reduce([]) do |memo, p|
-      memo << p.years_language_experience[language]
+    .reduce(0) do |memo, p|
+      memo + p.years_language_experience[language]
     end
-    .reduce(&:+)
   end
 
   def person_with_most_experience_in_language(language)
-    @people.reject do |p|
+    people.reject do |p|
       p.years_language_experience[language] == nil
     end
-    .reduce do |p1, p2|
-      if p1.years_language_experience[language] > p2.years_language_experience[language]
-        p1
-      else
-        p2
-      end
+    .max_by do |p|
+      p.years_language_experience[language]
     end
   end
 
